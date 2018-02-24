1478bc30bb16d4a0d492de046a73f02030fcf019
ethiraj <ethirajsrinivasan@gmail.com>
2018-02-24 16:09:52 +0800

    indentation changes

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -17,10 +17,10 @@ RSpec.describe ProjectsController do
   end
 
   describe "GET #show" do
-  	it "renders the show template" do
+    it "renders the show template" do
       get :show, id: project.id
-  		expect(response).to render_template("show")
-  	end
+      expect(response).to render_template("show")
+    end
   end
 
   describe "GET #new" do
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,43 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Section, type: :model do
-	subject { Section.new}
+  subject { Section.new}
 
-	describe "Validations" do
-		it "is valid with valid attributes" do
-			subject.order = 1
-			subject.title = "My project"
-			subject.content = "<div>Hi</div>"
-			subject.project_id = 1
-			expect(subject).to be_valid
-		end
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      subject.order = 1
+      subject.title = "My project"
+      subject.content = "<div>Hi</div>"
+      subject.project_id = 1
+      expect(subject).to be_valid
+    end
 
-		it "is not valid without order" do
-			expect(subject).not_to be_valid
-		end
+    it "is not valid without order" do
+      expect(subject).not_to be_valid
+    end
 
-		it "is not valid without title" do
-			subject.order = 1
-			expect(subject).not_to be_valid
-		end
+    it "is not valid without title" do
+      subject.order = 1
+      expect(subject).not_to be_valid
+    end
 
-		it "is not valid without content" do
-			subject.order = 1
-			subject.title = "My project"
-			expect(subject).not_to be_valid
-		end
+    it "is not valid without content" do
+      subject.order = 1
+      subject.title = "My project"
+      expect(subject).not_to be_valid
+    end
 
-		it "is not valid without project_id" do
-			subject.order = 1
-			subject.title = "My project"
-			subject.content = "<div>Hi</div>"
-			expect(subject).not_to be_valid
-		end
-	end
+    it "is not valid without project_id" do
+      subject.order = 1
+      subject.title = "My project"
+      subject.content = "<div>Hi</div>"
+      expect(subject).not_to be_valid
+    end
+  end
 
-	describe "Associations" do
-		it {should belong_to(:project)}
-	end
-
+  describe "Associations" do
+    it {should belong_to(:project)}
+  end
 
 end
