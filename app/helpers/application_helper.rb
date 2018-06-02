cff4c11d7c51e5a09492018d636ea593746d539f
ethiraj <ethirajsrinivasan@gmail.com>
2018-06-02 16:47:42 +0800

    adjustable width

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+	def header_image
+		if params[:controller] == "blogs" && params[:action] == "index"
+			"blogs.jpg"
+		elsif params[:controller] == "home" 
+			case params[:action]
+			when "about"
+				"about.jpg"
+			when "contact"
+				"contact.jpg"
+			when "welcome"
+				"welcome.jpg"
+			else
+				"welcome.jpg"
+			end
+		else
+			"welcome.jpg"
+		end
+	end
+
+	def header_height
+		if params[:controller] == "home" && params[:action] == "welcome"
+			"51rem"
+		else
+			"30rem"
+		end
+	end
 end
